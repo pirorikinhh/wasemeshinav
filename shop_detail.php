48f913c2ad3643f01835a9b240d19f0e9476b9cc
pirorikinhh <pirorikinhh@gmail.com>
2015-08-25 17:38:48 +0900

    Update shop_detail.php
    グッドボタン追加

--- shop_detail.php
+++ shop_detail.php
@@ -1,24 +1,37 @@
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-<title>店舗詳細</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-62034440-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+    <title>店舗詳細</title>
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/hammer.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-62034440-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <style>
+        .countbtn {
+             width: 100%;
+             padding: 7px;
+             border-radius: 5px;
+             box-shadow: 0 4px 0 #0088cc;
+             color: #fff;
+             background: #00aaff;
+             cursor: pointer;
+             opacity: 1.0;
+             text-align: center;
+          }
+     </style>
 </head>
 <body>
 
-<!--ヘッダーここから--!>
 <div class="container">
 <div class="common-header">
         <div class="row">
@@ -30,23 +43,25 @@
       </div>
 </div>
 </div>
-<!--ヘッダーここまで--!>
+
 
 <HR>
 
-<?php 
+<?php
 
 include('db_connect.php');
 
 try {
 if (!isset($_REQUEST['id']) || !is_string($id = $_REQUEST['id'])) { throw new RuntimeException('コードを指定 してください'); }
 
+            $id=$_REQUEST['id'];
+
 			//infosql
 			$sql = 'select * from wasemeshi_list where id=?';
 			$stmt = $pdo->prepare($sql);
 			$stmt->bindValue(1, $id, PDO::PARAM_INT);
 			$stmt->execute();
-			foreach ($stmt as $info){} 
+			foreach ($stmt as $info){}
 
 			//menussql
 			$sql = 'select * from menu_list where id=?';
@@ -128,7 +143,12 @@ print<<<EOM
 								<h3 class="text-center">{$info['name']}</h3>
 							</div>
 						</div>
-                                                 <hr>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div type="button" class="countbtn"><span class="glyphicon glyphicon-heart"></span><div class="count"></div></div>
+                            </div>
+                        </div>
 						<div class="row">
 							<div class="col-md-12 col-xs-12">
 								<h5 class="text-center">{$info['comment']}</h5>
@@ -150,68 +170,72 @@ print<<<EOM
 							   <tr>
 							      <td width="35%">門</td>
 							      <td width="65%">{$info[gate]}</td>
-							   </tr>  
+							   </tr>
 							   <tr>
 							      <td width="35%">価格帯</td>
                   					      <td width="65%">{$info[price]}円～</td>
 							   </tr>
 							 </tbody>
 							</table>
-                                                <div class="row">
-							<div class="col-md-6">
-								<a class="btn btn-primary btn-product"><span class="glyphicon glyphicon-thumbs-up"></span> Like</a> 
-							</div>
-						</div>
-
 						<p> </p>
                                 <div class="container">
                                       <p>{$info['address']}</p>
                                       <iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.co.jp/maps?q={$info['address']}&z=16&output=embed&iwloc=J&t=m"></iframe></div>
 				</div>
 			</div>
-        </div> 
+        </div>
 	</div>
 </div>
 
 EOM;
 ?>
 
-
-
-
-<!--フッターここから-->
      <div id="footer">
-
-     <ul class="breadcrumb" style="margin-bottom: 5px;">
-       <li class="active"><a href="http://www.wasemeshi.com">Home</a></li>
-       <li><a href="http://www.wasemeshi.com/aboutus.html">Profile</a></li>
-       <li><a href="http://www.wasemeshi.com/contact.html">contact</a></li>
-       <li><a href="http://www.wasemeshi.com/blog_list.php">みにれぽ</a></li>
-     </ul>
-<br/>
-      Copyrightc <a href="http://www.wasemeshi.com">ワセメシなび</a>, All rights reserved.<br>
-
+             <ul class="breadcrumb" style="margin-bottom: 5px;">
+               <li class="active"><a href="http://www.wasemeshi.com">Home</a></li>
+               <li><a href="http://www.wasemeshi.com/aboutus.html">Profile</a></li>
+               <li><a href="http://www.wasemeshi.com/contact.html">contact</a></li>
+               <li><a href="http://www.wasemeshi.com/blog_list.php">みにれぽ</a></li>
+             </ul><br/>
+              Copyrightc <a href="http://www.wasemeshi.com">ワセメシなび</a>, All rights reserved.<br>
      </div>
-<!--フッターここまで-->
 
-
-<!--scriptここから-->
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/hammer.min.js"></script>
-
-    <script>
-     $(function()
-      {
-      var carousel = $('#carousel');
-      var hammer = new Hammer(carousel[0]);
-      hammer.on('swipeleft', function(){ carousel.carousel('next'); });
-      hammer.on('swiperight', function(){ carousel.carousel('prev'); });
-      });
+    <script>//スライドショー
+             $(function()
+              {
+              var carousel = $('#carousel');
+              var hammer = new Hammer(carousel[0]);
+              hammer.on('swipeleft', function(){ carousel.carousel('next'); });
+              hammer.on('swiperight', function(){ carousel.carousel('prev'); });
+              });
     </script>
 
+    <script>//ajax通信
+            var id = <?php print $id; ?>;
+            $(document).ready(function() {
+                 ajaxstart('ready',id);
+            });
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+            $(function(){
+                jQuery(".countbtn").click(function() {
+                        ajaxstart('countup',id);
+                        console.log('countup'); 
+                });
+            });
+
+
+            function ajaxstart(act,id){
+                    $.ajax({
+                        type: "POST",
+                        url: "goodcount.php?action="+act+"&id="+id
+                        })
+                        .done(function(data){
+                            if(data.length > 0){
+                                $(".count").text("→ "+data);
+                            }
+                        });
+            }
+    </script>
+
 </body>
 </html>
